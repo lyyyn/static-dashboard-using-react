7278eca90d8fad8eb41ac40f1e6be6f195d33d33
Linawaty <linawatylim@gmail.com>
2020-07-04 15:20:27 +0800

    statis dashboard

--- app.js
+++ app.js
@@ -1,4 +1,89 @@
+const LeftMenu = () => {
+  return (
+    <div className='sidebar'>
+      <ul>
+        <li>Dashboard</li>
+        <li>Widgets</li>
+        <li>Reviews</li>
+        <li>Customers</li>
+        <li>Online Analysis</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  )
+}
+
+const Review = () => {
+  return (
+    <div className='widget'>
+      <div>Review</div>
+      <div className='largeFont'>1,267</div>
+    </div>
+  )
+}
+
+const Ratings = () => {
+  return (
+    <div className='widget'>
+      <div>Average Ratings</div>
+      <div className='largeFont'>4.6</div>
+    </div>
+  )
+}
+
+const Sentiments = () => {
+  return (
+    <div className='widget'>
+      <div>Sentiments Analysis</div>
+      <div className='largeFont'>960<br/>122<br/>321</div>
+    </div>
+  )
+}
+
+const Chart = () => {
+  return (
+    <div className='visitors-chart'>Chart here</div>
+  )
+}
+
+const Visitors = () => {
+  return (
+    <div className='visitors'>
+      <div>Website Visitors</div>
+      <div className='largeFont'>821</div>
+      <Chart />
+    </div>
+  )
+}
+
+const TopWidgets = () => {
+  return (
+    <div className='topWidgets'>
+      <Review />
+      <Ratings />
+      <Sentiments />
+    </div>
+  )
+}
+
+const Header = () => {
+  return (
+    <div className='header'><h1>Commence Dashboard Creation!</h1></div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className='main'>
+      <Header />
+      <LeftMenu />
+      <TopWidgets />
+      <Visitors />
+    </div>
+  )
+}
+
 ReactDOM.render(
-    <h1>Commence Dashboard Creation!</h1>,
-    document.querySelector('main')
-  );
+  <App />,
+  document.querySelector('main')
+);
